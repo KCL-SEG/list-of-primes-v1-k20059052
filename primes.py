eb5dd83fb44bd67a6c4f8e7b9b931108d688388e
k20059052 <101188329+k20059052@users.noreply.github.com>
2022-09-28 19:41:51 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -2,5 +2,23 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    
+    list = [] 
+    prime = True 
+    base_number = 2
+    while len(list) != number_of_primes:
+      
+        for divisor in range(2,base_number):
+            prime = True
+            if(base_number % divisor) == 0:
+                prime = False
+                break
+            
+        if prime == True:
+            list.append(base_number)
+    
+        
+        
+        base_number +=1
+    
     return list
